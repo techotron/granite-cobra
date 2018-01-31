8d9b5683bf2d03795b39d1f50d327ae2cc86f3b0
xlpnic <nic.foley5@gmail.com>
2018-01-31 23:13:42 +0000

    Add basic card object and set visibility using button

--- CobraAndroid/app/src/main/java/granite_cobra/cobraandroid/MainActivity.java
+++ CobraAndroid/app/src/main/java/granite_cobra/cobraandroid/MainActivity.java
@@ -1,7 +1,9 @@
 package granite_cobra.cobraandroid;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,10 +11,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setCardVisible(false);
     }
 
     private Card getCard(){
         return new Card();
+    }
+
+    public boolean receivedACard;
+
+    public void getACard(View view) {
+
+        Card cardReceived = getCard();
+
+        receivedACard = true;
+
+        setCardVisible(true);
+    }
+
+    private void setCardVisible(boolean setToVisible) {
+        ConstraintLayout cardContainer = findViewById(R.id.cardContainer);
+
+        if(setToVisible){
+            cardContainer.setVisibility(View.VISIBLE);
+        }
+        else{
+            cardContainer.setVisibility(View.INVISIBLE);
+        }
     }
 }
 
